97ffb94fe593c6d68c2a277afe3bca437d5b0e13
kingOfSchottland <46346267+kingOfSchottland@users.noreply.github.com>
2024-10-21 17:36:01 +0900

    add content edit

--- lib/screen/alarm_setting_screen.dart
+++ lib/screen/alarm_setting_screen.dart
@@ -1,18 +1,23 @@
 import 'dart:ffi';
+import 'dart:io';
 
 import 'package:_night_alarm_manager/widget/alarm_days_widget.dart';
 import 'package:_night_alarm_manager/widget/alarm_time_widget.dart';
+import 'package:_night_alarm_manager/widget/text_message_input_widget.dart';
 import 'package:flutter/material.dart';
 
 class AlarmSettingScreen extends StatefulWidget {
   final String type;
   late String alarmTime;
+  late String textMessage;
   late List<String> alarmDays = [];
+  late File? selectedFile = File('');
 
   AlarmSettingScreen({
     super.key,
     required this.type,
     this.alarmTime = '',
+    this.textMessage = '',
   });
 
   @override
@@ -31,6 +36,18 @@ class _AlarmSettingScreenState extends State<AlarmSettingScreen> {
       widget.alarmDays.contains(day)
           ? widget.alarmDays.remove(day)
           : widget.alarmDays.add(day);
+    });
+  }
+
+  void setTextMessage(String message) {
+    setState(() {
+      widget.textMessage = message;
+    });
+  }
+
+  void setSelectedFile(File file) {
+    setState(() {
+      widget.selectedFile = file;
     });
   }
 
@@ -59,17 +76,64 @@ class _AlarmSettingScreenState extends State<AlarmSettingScreen> {
               fontWeight: FontWeight.w600),
         ),
       ),
-      body: Column(
-        children: [
-          AlarmTime(setAlarmTime: setAlarmTime),
-          const SizedBox(
-            height: 5.0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: Column(
+            children: [
+              AlarmTime(setAlarmTime: setAlarmTime),
+              const SizedBox(
+                height: 5.0,
+              ),
+              AlarmDaysWidget(
+                alarmDays: widget.alarmDays,
+                setAlarmDays: setAlarmDays,
+              ),
+              MessageInputWidget(
+                type: widget.type,
+                textMessage: widget.textMessage,
+                setTextMessage: setTextMessage,
+                setSelectedFile: setSelectedFile,
+              ),
+              widget.selectedFile == null
+                  ? const SizedBox()
+                  : Text(widget.selectedFile!.path,
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Noto_Sans_KR',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400)),
+              const SizedBox(
+                height: 15.0,
+              ),
+              GestureDetector(
+                onTap: () {
+                  // submit code
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 7.0),
+                  width: 300,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6.0),
+                      color: const Color(0xff3ad277)),
+                  child: const Text(
+                    '작성 완료',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Noto_Sans_KR',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+            ],
           ),
-          AlarmDaysWidget(
-            alarmDays: widget.alarmDays,
-            setAlarmDays: setAlarmDays,
-          )
-        ],
+        ),
       ),
     );
   }
---
+++ lib/widget/text_message_input_widget.dart
@@ -0,0 +1,171 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class MessageInputWidget extends StatefulWidget {
+  final String type;
+  final String textMessage;
+  final Function setTextMessage;
+  final Function setSelectedFile;
+
+  const MessageInputWidget(
+      {super.key,
+      required this.type,
+      required this.textMessage,
+      required this.setTextMessage,
+      required this.setSelectedFile});
+
+  @override
+  State<MessageInputWidget> createState() => _MessageInputWidgetState();
+}
+
+class _MessageInputWidgetState extends State<MessageInputWidget> {
+  final TextEditingController controller = TextEditingController();
+  String? _selectedFileName;
+
+  // 권한 요청 함수
+  Future<bool> _requestPermission() async {
+    // PermissionStatus status = await Permission.storage.status;
+
+    // if (!status.isGranted) {
+    //   status = await Permission.storage.request();
+    // }
+    if (await Permission.storage.isGranted) {
+      return true;
+    }
+
+    // Android 11 이상에서는 MANAGE_EXTERNAL_STORAGE 권한이 필요
+    if (await Permission.storage.request().isGranted) {
+      return true;
+    } else if (await Permission.manageExternalStorage.request().isGranted) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // 파일 선택 함수
+  Future<void> _pickFile() async {
+    // 권한 체크
+    bool hasPermission = await _requestPermission();
+    if (!hasPermission) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("파일 접근 권한이 필요합니다.")),
+      );
+      return;
+    }
+
+    // 파일 선택
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null && result.files.isNotEmpty) {
+      setState(() {
+        _selectedFileName = result.files.first.name;
+      });
+
+      if (result.files.isNotEmpty) {
+        File file = File(result.files.first.path!);
+
+        widget.setSelectedFile(file);
+      }
+
+      // 여기서 파일을 서버로 업로드하는 로직을 추가할 수 있습니다.
+      print("Selected file: ${result.files.first.name}");
+    } else {
+      // 사용자가 파일 선택을 취소한 경우
+      print("No file selected");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xfffafafa),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              const SizedBox(
+                width: 20.0,
+              ),
+              Text(
+                '${widget.type.split(" ")[0]} Message',
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Noto_Sans_KR',
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          widget.type == 'Text Group'
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: TextField(
+                    onChanged: (text) {
+                      widget.setTextMessage(text);
+                    },
+                    controller: controller,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 0.0, horizontal: 12.0),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                        color:
+                            const Color(0xff898585).withOpacity(0.2), // 테두리 색상
+                        width: 0.5, // 테두리 두께
+                      )),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0), // 모서리 둥글게 설정
+                        borderSide: BorderSide(
+                          color: const Color(0xff898585)
+                              .withOpacity(0.2), // 테두리 색상
+                          width: 0.5, // 테두리 두께
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0), // 모서리 둥글게 설정
+                        borderSide: BorderSide(
+                          color: const Color(0xff898585)
+                              .withOpacity(0.2), // 테두리 색상
+                          width: 0.5,
+                        ),
+                      ),
+                      hintText: 'write down the text message.',
+                      hintStyle: TextStyle(
+                          color: const Color(0xff898585).withOpacity(0.6),
+                          fontFamily: 'Noto_Sans_KR',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                )
+              : GestureDetector(
+                  onTap: _pickFile,
+                  child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: const Color(0xffcbcbcbcb),
+                    ),
+                    child: const Icon(
+                      Icons.upload_rounded,
+                      color: Color(0xff888888),
+                      size: 28,
+                    ),
+                  ),
+                ),
+        ],
+      ),
+    );
+  }
+}
